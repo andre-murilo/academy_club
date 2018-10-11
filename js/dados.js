fd488c22916ee0794a64cdf30313f1b04d138900
wallace barbosa <43706747+wallacebarbosa@users.noreply.github.com>
2018-10-11 10:47:24 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,60 @@
+var db = {
+   
+    "dados": [
+        {
+
+         "img": "https://orofacial.com.br/files/2014/08/Slide6_Web.jpg",
+         "nome": "leonina",
+         "idade": "26",
+         "faculdade": "puc",
+         "curso": "direito",
+         "cidade": "belo horizonte",
+         "genero": "feminino",
+         "email": "leonina@gmail.com",
+        },
+
+        {
+
+            "img": "https://orofacial.com.br/files/2014/08/Slide6_Web.jpg",
+            "nome": "marcos",
+            "idade": "19",
+            "faculdade": "una",
+            "curso": "enfermagem",
+            "cidade": "belo horizonte",
+            "genero": "masculino",
+            "email": "marcolfk@gmail.com",
+           },
+
+           {
+
+            "img": "https://orofacial.com.br/files/2014/08/Slide6_Web.jpg",
+            "nome": "marcilene",
+            "idade": "22",
+            "faculdade": "pitagoras",
+            "curso": "engenharia civil",
+            "cidade": "belo horizonte",
+            "genero": "feminino",
+            "email": "marcilene@gmail.com",
+           }
+
+        
+        
+    ],
+
+    "interesses": [
+        {
+         
+        "int1": [{ 'nome': 'tabulero', 'descrição': '', 'sub-interesses': ['dama','xadrez','war'] }]
+        
+
+    }
+
+    ],
+
+    "grupos": [{
+
+        'taboleiro': ['xadrez', 'dama', 'war' ],
+    
+    }]
+    
+}
