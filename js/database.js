fea7b2994f9e381a0a8fd47094d2750204c0c978
Andre Murilo <35697459+andre-murilo@users.noreply.github.com>
2018-11-10 19:09:54 -0200

    Add files via upload

--- js/database.js
+++ js/database.js
@@ -1,52 +1,55 @@
-var config = {
-    apiKey: "AIzaSyC-gmxXyEg9q10C-HeYaGFQnhBrWt3QF74",
-    authDomain: "academyclub-fae14.firebaseapp.com",
-    databaseURL: "https://academyclub-fae14.firebaseio.com",
-    projectId: "academyclub-fae14",
-    storageBucket: "academyclub-fae14.appspot.com",
-    messagingSenderId: "1065301796384"
-};
-
-var defaultDatabase;
-
-function InitializeDB()
+class DatabaseManager
 {
-    var defaultApp = firebase.initializeApp(config);
-    defaultDatabase = defaultApp.database();
+    constructor()
+    {
+        this.event = new Event("OnConnected");
 
 
-    firebase.auth().signInAnonymously().catch(function(error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-      });
-
-
-
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            console.log("Logado com sucesso: " + uid);
-        } else {
-
+        this.config = {
+            apiKey: "AIzaSyC-gmxXyEg9q10C-HeYaGFQnhBrWt3QF74",
+            authDomain: "academyclub-fae14.firebaseapp.com",
+            databaseURL: "https://academyclub-fae14.firebaseio.com",
+            projectId: "academyclub-fae14",
+            storageBucket: "academyclub-fae14.appspot.com",
+            messagingSenderId: "1065301796384"
         }
-      });
-
-
-      defaultDatabase.ref("usuarios").on("value", function(snap) {
-        var usuarios = snap.val();
-
-        for(var i = 0; i < usuarios.length; i++)
-        {
-            console.log(usuarios[i].nome);
-        }
-
-      });
+    }
 
 
 
-    console.log(defaultApp.name); 
+    Initialize()
+    {
+        this.defaultApp = firebase.initializeApp(this.config);
+        this.defaultDB = this.defaultApp.database();
+    }
 
 
+    DoAuth()
+    {
+        firebase.auth().signInAnonymously().catch(function(error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+
+        firebase.auth().onAuthStateChanged(function(user) {
+            if(user)
+            {
+                var isAnonymous = user.isAnonymous;
+                var uid = user.uid;
+                console.log("[Database] Autenticado com sucesso: " + uid);
+            }
+            else
+            {
+                console.log("[Database] Desconectado!");
+            }
+
+        });
+    }
+
+    AddListernerUsers(func)
+    {
+        this.defaultDB.ref("usuarios").on("value", func);
+    }
 }
+
+
